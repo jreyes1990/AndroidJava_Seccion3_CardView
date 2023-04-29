38559d00e2fcf4d0516f98de87afbf40a3d0cd6b
jreyes1990 <jarl1990mt@gmail.com>
2023-04-28 23:32:26 -0600

    Uso de CardView - Ripple.

--- app/src/main/java/com/example/seccion3_cardview/MainActivity.java
+++ app/src/main/java/com/example/seccion3_cardview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.seccion3_cardview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    TextView textView = (TextView) findViewById(R.id.textViewTitle);
+    textView.setText("Hola bienvenidos al mundo de programadores");
   }
 }
